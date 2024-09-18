1870d3782a9b22f70066b837fa530b477c29e702
Sonal <sk5292294@gmail.com>
2024-09-19 02:32:55 +0530

    Interchanged videos

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,11 +24,11 @@ const HeroSection = () => {
         </div>
         <div className="flex mt-10 justify-center">
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video1} type="video/mp4"/>
+                <source src={video3} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video3} type="video/mp4"/>
+                <source src={video1} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
         </div>
