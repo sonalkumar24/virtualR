f4b0d93284d186b22ebb966fc3a879729b239054
Sonal <sk5292294@gmail.com>
2024-09-19 02:42:18 +0530

    Back with original videos because the compressed ones dont even play on my mobile.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
-//import video1 from "../assets/video1.mp4"
-import video4 from "../assets/video4.mp4"
+import video1 from "../assets/video1.mp4"
+import video2 from "../assets/video2.mp4"
 
 const HeroSection = () => {
   return (
@@ -23,12 +23,12 @@ const HeroSection = () => {
             </a>
         </div>
         <div className="flex mt-10 justify-center">
-            {/* <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
                 <source src={video1} type="video/mp4"/>
                 Your browser does not Support the video tag.
-            </video> */}
+            </video>
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video4} type="video/mp4"/>
+                <source src={video2} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
         </div>
