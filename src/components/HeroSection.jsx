f500a1f53ab612d28415e6f2b8ed8cf60ff74deb
Sonal <sk5292294@gmail.com>
2024-09-19 01:59:34 +0530

    Updated video elements for responsiveness

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -23,13 +23,21 @@ const HeroSection = () => {
             </a>
         </div>
         <div className="flex mt-10 justify-center">
-            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            {/* <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
                 <source src={video1} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
                 <source src={video2} type="video/mp4"/>
                 Your browser does not Support the video tag.
+            </video> */}
+            <video autoPlay loop muted className="rounded-lg border border-orange-700 shadow-orange-400 mx-2 my-4 w-full h-auto max-w-md">
+                <source src={video1} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <video autoPlay loop muted className="rounded-lg border border-orange-700 shadow-orange-400 mx-2 my-4 w-full h-auto max-w-md">
+                <source src={video2} type="video/mp4" />
+                Your browser does not support the video tag.
             </video>
         </div>
     </div>
