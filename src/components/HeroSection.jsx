b044e98266df72a496ecca51f3717f9436d897ea
Sonal <sk5292294@gmail.com>
2024-09-19 02:36:25 +0530

    Compressed 2nd video

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import video1 from "../assets/video1.mp4"
-import video3 from "../assets/video3.mp4"
+import video4 from "../assets/video4.mp4"
 
 const HeroSection = () => {
   return (
@@ -24,11 +24,11 @@ const HeroSection = () => {
         </div>
         <div className="flex mt-10 justify-center">
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video3} type="video/mp4"/>
+                <source src={video1} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video1} type="video/mp4"/>
+                <source src={video4} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
         </div>
