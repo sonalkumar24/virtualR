ad4840ae2b60f8ec8f37a537ecf0d10923e5f72e
Sonal <sk5292294@gmail.com>
2024-09-19 02:26:41 +0530

    Changed the second video

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import video1 from "../assets/video1.mp4"
-import video2 from "../assets/video2.mp4"
+import video3 from "../assets/video3.mp4"
 
 const HeroSection = () => {
   return (
@@ -28,7 +28,7 @@ const HeroSection = () => {
                 Your browser does not Support the video tag.
             </video>
             <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
-                <source src={video2} type="video/mp4"/>
+                <source src={video3} type="video/mp4"/>
                 Your browser does not Support the video tag.
             </video>
         </div>
